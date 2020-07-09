1c39a090c939cb2eec066c01002922101084a7c2
dzhelek <38794411+dzhelek@users.noreply.github.com>
2020-07-09 18:54:30 +0300

    'create movies' added

--- webapp/rdb/urls.py
+++ webapp/rdb/urls.py
@@ -1,6 +1,17 @@
-from django.urls import path
+from django.urls import path, include
 
-from . import views
+from rdb.views import index, movies
+
+app_name = 'rdb'
+
+movies_patterns = [
+    path('', movies.list, name='list'),
+    path('<int:movie_id>/', movies.detail, name='detail'),
+    path('new/', movies.Create.as_view(), name='create'),
+    path('<int:pk>/edit/', movies.Update.as_view(), name='edit'),
+]
 
 urlpatterns = [
+    path('', index, name='index'),
+    path('movies/', include((movies_patterns, 'movies'))),
 ]
