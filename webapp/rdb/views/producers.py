e0816019fd9f4178f3a71a7b6bf8d9fde1b15f0d
dzhelek <38794411+dzhelek@users.noreply.github.com>
2020-07-10 14:42:51 +0300

    'update producer' added

--- webapp/rdb/views/producers.py
+++ webapp/rdb/views/producers.py
@@ -4,22 +4,30 @@ from django.urls import reverse_lazy
 
 from rdb.models import Producer
 
+
 def list(request):
     return render(request, 'producers/list.html', {'producers': Producer.objects.all()})
+
 
 def delete(request, producer_id):
     if request.method == 'POST':
         Producer.objects.get(id=producer_id).delete()
     return redirect(reverse_lazy('rdb:producers:list'))
 
-class Create(CreateView):
+
+class Form():
     model = Producer
     fields = ['name', 'address', 'networth']
-    template_name = 'producers/create.html'
+    template_name = 'form.html'
 
     def get_success_url(self, **kwargs):
         return reverse_lazy('rdb:producers:list')
 
-class Update(UpdateView):
-    pass
+
+class Create(Form, CreateView):
+    template_name = 'producers/create.html'
+
+
+class Update(Form, UpdateView):
+    template_name = 'producers/edit.html'
 
